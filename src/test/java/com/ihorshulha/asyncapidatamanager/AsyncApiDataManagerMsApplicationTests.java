bbe25bf9cb473ea8c73e90de96b2e153d2731c8b
IgorShulga <igor.shulga8@gmail.com>
2023-08-09 13:21:56 +0200

    Initial commit

---
+++ src/test/java/com/ihorshulha/asyncapidatamanager/AsyncApiDataManagerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihorshulha.asyncapidatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApiDataManagerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
