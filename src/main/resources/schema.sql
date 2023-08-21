d8f96ed8887b3659c407cc21641f6c8f88adfcdb
IgorShulga <igor.shulga8@gmail.com>
2023-08-21 12:25:00 +0200

    edit schemas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,13 +17,3 @@ create TABLE IF NOT EXISTS stock (
     volume INT,
     company_name VARCHAR(255) NOT NULL
     );
-
---stock_audit_log
-CREATE TABLE IF NOT EXISTS stock_audit_log (
-    id serial PRIMARY KEY,
-    symbol VARCHAR(255) NOT NULL,
-    old_row_data jsonb,
-    new_row_data jsonb,
-    dml_type dml_type NOT NULL,
-    dml_timestamp timestamp NOT NULL
-);
